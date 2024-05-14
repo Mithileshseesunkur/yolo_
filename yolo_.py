11ae1742577ffe6fd1e60dc05a77df7af0fb4cbf
mithilesh <zavallingum@gmail.com>
2024-05-14 12:32:00 +0200

    still with turn on off

--- yolo_.py
+++ yolo_.py
@@ -8,10 +8,11 @@ width, height = 720,480
 vid.set(cv2.CAP_PROP_FRAME_WIDTH,width)
 vid.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
 
-webcam_running=None
+webcam_running= False
 
 
 class root(customtkinter.CTk):
+
     def __init__(self):
         super().__init__()
 
@@ -39,52 +40,65 @@ class root(customtkinter.CTk):
         self.button = customtkinter.CTkButton(self.webcam_button_frame, text="Turn off", command=self.webcam_off)
         self.button.grid(row=1, column=1,padx=(0,10), pady=(10), sticky="w")
 
-        webcam_running=True
 
     # turn on webcam
     def webcam_on(self):
-        global webcam_running
-        webcam_running=True
-        if webcam_running:
+        global webcam_running, vid
         
-            print("webcam on")
-            #capture video fram by frame
-            _, frame=vid.read()
+        if not webcam_running: #only turn on if webcam if off
+            webcam_running= True
+            print("Webcam on")
 
-            #convert image from one colour space to another
-            opencv_image= cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+        
 
-            #capture the latest frame and tansform to image
-            captured_image= Image.fromarray(opencv_image)
 
-            #convert captured image to photoimage
-            self.photo_image= customtkinter.CTkImage(light_image=captured_image,size=(width,height))
+        #capture video fram by frame
+        # try to get the first frame
+    
+        ret, frame=vid.read() # read a frame from the webcam
+        print("webcam ruuning", webcam_running)
 
-            #displaying the photoimage in the label
-            self.video_frame.photo_image=self.photo_image
+        
 
-            #configue image in the label
-            self.video_label.configure(image=self.photo_image)
+        #convert image from one colour space to another
+        opencv_image= cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
 
-            #repeat the same process every 10 seconds
-            
-            self.video_label.after(10, self.webcam_on)
+        #capture the latest frame and tansform to image
+        captured_image= Image.fromarray(opencv_image)
 
-    # turn off webcam      
+        #convert captured image to photoimage
+        self.photo_image= customtkinter.CTkImage(light_image=captured_image,size=(width,height))
+
+        #displaying the photoimage in the label
+        self.video_frame.photo_image=self.photo_image
+
+        #configue image in the label            
+        self.video_label.configure(image=self.photo_image)
+
+        #repeat the same process every 10 seconds
+        #self.video_label.after(10, self.webcam_on)
+
+        # handle potential keyboard interrupt 
+        if cv2.waitKey(10) == ord('q'):
+            break
+
+
+    # turn off webcam      qq
     def webcam_off(self):
         
         global webcam_running
-       # self.video_label.after_cancel(self.video_feed)
-       # vid.release()
+        
+        if webcam_running: # only turn off if webcam is on
+            webcam_running= False
+
+        #stop scheduled frame update
+        self.video_label.after_cancel(self.webcam_on)
+
+        # release the webcam recourses
+        #if vid.isOpened():
+            #vid.release()
         #webcam_running=False
-        print("in webcam_off now")
-
-
-
-
-
-
-
+        print("in webcam_off now",webcam_running)
 
 
 
